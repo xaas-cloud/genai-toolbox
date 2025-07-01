27f2c50f73519de9d4a890d5c48a057e6902c94e
duwenxin <duwenxin@google.com>
2025-07-01 17:31:42 -0400

    add test config

--- cmd/root.go
+++ cmd/root.go
@@ -65,7 +65,6 @@ import (
 	_ "github.com/googleapis/genai-toolbox/internal/tools/redis"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/spanner/spannerexecutesql"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/spanner/spannersql"
-	_ "github.com/googleapis/genai-toolbox/internal/tools/redis"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/sqlitesql"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/utility/wait"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/valkey"
