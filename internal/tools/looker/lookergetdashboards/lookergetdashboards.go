00866bc7fc33115c547213e60316ae889735fdbb
Dr. Strangelove <drstrangelove@google.com>
2025-08-26 16:32:08 -0400

    fix(tools/looker): lookergetdashboards uses proper Authorized helper func (#1255)
    looker-getdash-board uses the Authorized helper function.
    
    ## PR Checklist
    ---
    > Thank you for opening a Pull Request! Before submitting your PR, there
    are a
    > few things you can do to make sure it goes smoothly:
    - [x ] Make sure you reviewed
    
    [CONTRIBUTING.md](https://github.com/googleapis/genai-toolbox/blob/main/CONTRIBUTING.md)
    - [ ] Make sure to open an issue as a
    
    [bug/issue](https://github.com/googleapis/langchain-google-alloydb-pg-python/issues/new/choose)
    before writing your code! That way we can discuss the change, evaluate
      designs, and agree on the general idea
    - [ x] Ensure the tests and linter pass
    - [ x] Code coverage does not decrease (if any source code was changed)
    - [ x] Appropriate docs were updated (if necessary)
    - [x ] Make sure to add `!` if this involve a breaking change

--- internal/tools/looker/lookergetdashboards/lookergetdashboards.go
+++ internal/tools/looker/lookergetdashboards/lookergetdashboards.go
@@ -184,7 +184,7 @@ func (t Tool) McpManifest() tools.McpManifest {
 }
 
 func (t Tool) Authorized(verifiedAuthServices []string) bool {
-	return true
+	return tools.IsAuthorized(t.AuthRequired, verifiedAuthServices)
 }
 
 func (t Tool) RequiresClientAuthorization() bool {
